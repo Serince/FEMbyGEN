0b1d92f903b617f68b9dafd3ac4fb5ba480dee26
Pakrad <pakradamini@gmail.com>
2024-04-24 14:55:15 +0300

    massage

--- Init.py
+++ Init.py
@@ -1,4 +1,8 @@
 import FreeCAD
+import FreeCADGui
+
+FreeCADGui.addLanguagePath(os.path.join(FreeCAD.getUserAppDataDir(),"\Mod\FEMbyGEN\fembygen\translations"))
+
 translate = FreeCAD.Qt.translate
 print(translate("FEMbyGEN","FEMbyGEN Workbench loaded"))
 #try101
--- fembygen/translations/MyModule.ts
+++ fembygen/translations/MyModule.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="tr_TR">
 <context>
     <name>CommandName</name>
     <message>
@@ -64,7 +64,7 @@
     <message>
         <location filename="../topology/beso_main.py" line="320"/>
         <source>!!!!!! Domains to filter !!!!!!</source>
-        <translation type="unfinished"></translation>
+        <translation>!!!!!!filtrelenecek domainle!!!!!!</translation>
     </message>
     <message>
         <location filename="../topology/beso_main.py" line="491"/>
