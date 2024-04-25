8c4e0b1054725062677e4d8fb705bbfe3b99e3fb
Pakrad <pakradamini@gmail.com>
2024-04-25 18:01:06 +0300

    massageeee

--- Init.py
+++ Init.py
@@ -1,8 +1,6 @@
 import FreeCAD
 import FreeCADGui
 
-FreeCADGui.addLanguagePath(os.path.join(FreeCAD.getUserAppDataDir(),"\Mod\FEMbyGEN\fembygen\translations"))
-
 translate = FreeCAD.Qt.translate
 print(translate("FEMbyGEN","FEMbyGEN Workbench loaded"))
 #try101
